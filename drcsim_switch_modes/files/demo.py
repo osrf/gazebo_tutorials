c5b6e36c12ad2d616c3dfa32e22bed723e125274
Steve Peters <scpeters@osrfoundation.org>
2014-09-09 17:36:12 -0700

    Update drcsim_switch_modes tutorial, fix outdated joint names, remove initial step, remove gain setting, which was causing problems

--- drcsim_switch_modes/files/demo.py
+++ drcsim_switch_modes/files/demo.py
@@ -21,9 +21,9 @@ class Demo:
         self.imu_msg = None
 
         # Set up publishers / subscribers
-        self.ac_pub = rospy.Publisher('atlas/atlas_command', AtlasCommand)
+        self.ac_pub = rospy.Publisher('atlas/atlas_command', AtlasCommand, queue_size=1)
         self.asic_pub = rospy.Publisher('atlas/atlas_sim_interface_command', 
-          AtlasSimInterfaceCommand)
+          AtlasSimInterfaceCommand, queue_size=1)
         self.asis_sub = rospy.Subscriber('atlas/atlas_sim_interface_state', 
           AtlasSimInterfaceState, self.state_cb)
         self.imu_sub = rospy.Subscriber('atlas/imu', 
@@ -38,28 +38,6 @@ class Demo:
         self.imu_msg = msg
     
     def demo(self):
-        # Step 0: Go to home pose under user control
-        home_msg = AtlasCommand()
-        # Always insert current time
-        home_msg.header.stamp = rospy.Time.now()
-        # Assign some position and gain values that will get us there.
-        home_msg.position = [0.0] * self.NUM_JOINTS
-        home_msg.velocity = [0.0] * self.NUM_JOINTS
-        home_msg.effort = [0.0] * self.NUM_JOINTS
-        home_msg.kp_position = [20.0, 4000.0, 2000.0, 20.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0]
-        home_msg.ki_position = [0.0] * self.NUM_JOINTS
-        home_msg.kd_position = [0.0] * self.NUM_JOINTS
-        # Bump up kp_velocity to reduce the jerkiness of the transition
-        home_msg.kp_velocity = [50.0] * self.NUM_JOINTS
-        home_msg.i_effort_min = [0.0] * self.NUM_JOINTS
-        home_msg.i_effort_max = [0.0] * self.NUM_JOINTS 
-        # Set k_effort = [1] to indicate that we want all joints under user control
-        home_msg.k_effort = [255] * self.NUM_JOINTS
-        # Publish and give time to take effect
-        print('[USER] Going to home position...')
-        self.ac_pub.publish(home_msg)
-        time.sleep(2.0)
-    
         # Step 1: Go to stand-prep pose under user control
         stand_prep_msg = AtlasCommand()
         # Always insert current time
@@ -68,19 +46,11 @@ class Demo:
         stand_prep_msg.position = [2.438504816382192e-05, 0.0015186156379058957, 9.983908967114985e-06, -0.0010675729718059301, -0.0003740221436601132, 0.06201673671603203, -0.2333149015903473, 0.5181407332420349, -0.27610817551612854, -0.062101610004901886, 0.00035181696875952184, -0.06218484416604042, -0.2332201600074768, 0.51811283826828, -0.2762000858783722, 0.06211360543966293, 0.29983898997306824, -1.303462266921997, 2.0007927417755127, 0.49823325872421265, 0.0003098883025813848, -0.0044272784143686295, 0.29982614517211914, 1.3034454584121704, 2.000779867172241, -0.498238742351532, 0.0003156556049361825, 0.004448802210390568]
         stand_prep_msg.velocity = [0.0] * self.NUM_JOINTS
         stand_prep_msg.effort = [0.0] * self.NUM_JOINTS
-        stand_prep_msg.kp_position = [20.0, 4000.0, 2000.0, 20.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0]
-        stand_prep_msg.ki_position = [0.0] * self.NUM_JOINTS
-        stand_prep_msg.kd_position = [0.0] * self.NUM_JOINTS
-        # Bump up kp_velocity to reduce the jerkiness of the transition
-        stand_prep_msg.kp_velocity = [50.0] * self.NUM_JOINTS
-        stand_prep_msg.i_effort_min = [0.0] * self.NUM_JOINTS
-        stand_prep_msg.i_effort_max = [0.0] * self.NUM_JOINTS 
-        # Set k_effort = [1] to indicate that we want all joints under user control
         stand_prep_msg.k_effort = [255] * self.NUM_JOINTS
         # Publish and give time to take effect
         print('[USER] Going to stand prep position...')
         self.ac_pub.publish(stand_prep_msg)
-        time.sleep(2.0)
+        time.sleep(10.0)
     
         # Step 2: Request BDI stand mode
         stand_msg = AtlasSimInterfaceCommand()
@@ -112,11 +82,11 @@ class Demo:
         slight_movement_msg.header.stamp = rospy.Time.now()
         # Start with 0.0 and set values for the joints that we want to control
         slight_movement_msg.position = [0.0] * self.NUM_JOINTS
-        slight_movement_msg.position[AtlasState.neck_ay] = -0.1
+        slight_movement_msg.position[AtlasState.neck_ry] = -0.1
         slight_movement_msg.position[AtlasState.l_arm_ely] = 2.1
-        slight_movement_msg.position[AtlasState.l_arm_mwx] = -0.1
+        slight_movement_msg.position[AtlasState.l_arm_wrx] = -0.1
         slight_movement_msg.position[AtlasState.r_arm_ely] = 2.1
-        slight_movement_msg.position[AtlasState.r_arm_mwx] = -0.1
+        slight_movement_msg.position[AtlasState.r_arm_wrx] = -0.1
         slight_movement_msg.velocity = [0.0] * self.NUM_JOINTS
         slight_movement_msg.effort = [0.0] * self.NUM_JOINTS
         slight_movement_msg.kp_position = [20.0, 4000.0, 2000.0, 20.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 5.0, 100.0, 2000.0, 1000.0, 900.0, 300.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0, 2000.0, 1000.0, 200.0, 200.0, 50.0, 100.0]
@@ -129,11 +99,11 @@ class Demo:
         # Set k_effort = [1] for the joints that we want to control.
         # BDI has control of the other joints
         slight_movement_msg.k_effort = [0] * self.NUM_JOINTS
-        slight_movement_msg.k_effort[AtlasState.neck_ay] = 255
+        slight_movement_msg.k_effort[AtlasState.neck_ry] = 255
         slight_movement_msg.k_effort[AtlasState.l_arm_ely] = 255
-        slight_movement_msg.k_effort[AtlasState.l_arm_mwx] = 255
+        slight_movement_msg.k_effort[AtlasState.l_arm_wrx] = 255
         slight_movement_msg.k_effort[AtlasState.r_arm_ely] = 255
-        slight_movement_msg.k_effort[AtlasState.r_arm_mwx] = 255
+        slight_movement_msg.k_effort[AtlasState.r_arm_wrx] = 255
         # Publish and give time to take effect
         print('[USER/BDI] Command neck and arms...')
         self.ac_pub.publish(slight_movement_msg)
@@ -190,7 +160,7 @@ class Demo:
         # Publish and give time to take effect
         print('[USER/BDI] Walking...')
         self.asic_pub.publish(walk_msg)
-        time.sleep(6.0)
+        time.sleep(16.0)
 
         # Step 5: Go back to home pose under user control
         home_msg = AtlasCommand()
