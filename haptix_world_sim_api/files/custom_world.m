68b9796b7712a8847d3b0c7e7ee0f2bd4f4bd688
John Hsu <hsu@osrfoundation.org>
2015-08-11 15:33:32 -0700

    add inline comment clarifying the color 4-tuple definition.

--- haptix_world_sim_api/files/custom_world.m
+++ haptix_world_sim_api/files/custom_world.m
@@ -5,6 +5,16 @@
 hxs_set_model_collide_mode('sphere_visual_model', 1)
 
 while (true)
+  % note that the color vector is defined by the RGBA 4-tuple:
+  %
+  % https://s3.amazonaws.com/osrf-distributions/haptix/api/0.7.1/struct__hxsColor.html
+  %
+  % The elements are ordered such that:
+  %   color(1) = float value for the red component
+  %   color(2) = float value for the green component
+  %   color(3) = float value for the blue component
+  %   color(4) = float value for the alpha component
+  %
   color = [0;1;0;1];
   if length(hxs_contacts('sphere_visual_model')) > 0,
     color = [1;0;0;1];
